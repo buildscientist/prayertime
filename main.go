70a9f5932f019b884f8bd1681b3191fac41005f6
Youssuf ElKalay <buildscientist@users.noreply.github.com>
2017-09-20 23:06:54 -0700

    Added more examples of cities around the world - including Helsinki to factor in high elevation for Fajr and Isha prayer times.

--- main.go
+++ main.go
@@ -7,15 +7,30 @@ import (
 )
 
 func main() {
+	var helsinki = praytime.New(60.192059, 24.945831, 3)
+	var toronto = praytime.New(43.6532, -79.3832, -4.0)
 	var chicago = praytime.New(41.879626, -87.648217, -5.0)
 	var sandiego = praytime.New(32.7157, -117.1611, -7.0)
 
-	chicago.TimeFormat = praytime.TIME_24
-	sandiego.TimeFormat = praytime.TIME_24
+	helsinki.TimeFormat = praytime.TIME_24
+	helsinki.PrayerCalcMethod = praytime.MWL
+	helsinki.AdjustHighLats = praytime.ANGLE_BASED
+	helsinki.AsrJuristic = praytime.HANAFI
 
+	toronto.TimeFormat = praytime.TIME_24
+	toronto.AdjustHighLats = praytime.ANGLE_BASED
+	toronto.AsrJuristic = praytime.HANAFI
+
+	chicago.TimeFormat = praytime.TIME_12_NO_SUFFIX
+	sandiego.TimeFormat = praytime.TIME_12
+
+	var helsinkiPrayerTime = praytime.CalculatePrayerTimes(&helsinki, time.Now())
+	var torontoPrayerTime = praytime.CalculatePrayerTimes(&toronto, time.Now())
 	var sandiegoPrayerTime = praytime.CalculatePrayerTimes(&sandiego, time.Now())
 	var chicagoPrayerTime = praytime.CalculatePrayerTimes(&chicago, time.Now())
 
+	printPrayerTimes("Helsinki", helsinkiPrayerTime)
+	printPrayerTimes("Toronto", torontoPrayerTime)
 	printPrayerTimes("San Diego", sandiegoPrayerTime)
 	printPrayerTimes("Chicago", chicagoPrayerTime)
 
@@ -24,8 +39,8 @@ func main() {
 func printPrayerTimes(city string, prayertimes []string) {
 	var today = time.Now()
 	fmt.Println()
+	fmt.Println("=======" + city + "=======")
 	fmt.Println(today.Month(), today.Day(), today.Year())
-	fmt.Println("============" + city + "============")
 
 	for x := 0; x < len(prayertimes); x++ {
 		fmt.Println(praytime.PrayerTimeNames[x] + " - " + prayertimes[x])
